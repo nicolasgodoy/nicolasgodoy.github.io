171a08573a43daac6f4eae472ac92b84a66f61e5
nicolasgodoy <nicolasgodoy540@gmail.com>
2021-07-03 01:00:40 -0300

    actualizado

--- Modo-Oscuro.js
+++ Modo-Oscuro.js
@@ -1,23 +1,49 @@
 const boton = document.querySelector('#Modo-Oscuro');
+const boton1 = document.querySelector('#Modo-Dia');
+
 
 
 boton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
+    document.body.classList.add('dark-mode');
+    document.body.classList.remove('light-mode');
 
     var container = document.querySelector('.ContenedorHerramientas');
     container.classList.add('dark-mode');
+    var container = document.querySelector('.ContenedorHerramientas');
+    container.classList.remove('ContenedorHerramientas');
+
+    var container = document.querySelector('.ContenedorLenguajes');
+    container.classList.add('dark-mode');
+    var container = document.querySelector('.ContenedorLenguajes');
+    container.classList.remove('ContenedorLenguajes');
+
+
+    var container = document.querySelector('.ContenedorProyectos');
+    container.classList.add('dark-mode');
+    var container = document.querySelector('.ContenedorProyectos');
+    container.classList.remove('ContenedorProyectos');
+
+}),
+
+boton1.addEventListener('click', () => {
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
     
-    var container1 = document.querySelector('.ContenedorHerramientas');
-    container1.classList.remove('ContenedorHerramientas');
 
-    var container1 = document.querySelector('.ContenedorLenguajes');
-    container1.classList.remove('ContenedorLenguajes');
+    var container1 = document.querySelector('.dark-mode');
+    container1.classList.add('ContenedorHerramientas');
+    container1.classList.remove('dark-mode');
 
-    var container1 = document.querySelector('.ContenedorProyectos');
-    container1.classList.remove('ContenedorProyectos');
+    var container1 = document.querySelector('.dark-mode');
+    container1.classList.add('ContenedorLenguajes');
+    container1.classList.remove('dark-mode');
 
-   
-
-   
+    var container1 = document.querySelector('.dark-mode');
+    container1.classList.add('ContenedorProyectos');
+    container1.classList.remove('dark-mode');
 });
+
+
+
+
 
